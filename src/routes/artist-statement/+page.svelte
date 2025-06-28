e6de26fb37a6701ad3d63c7f28c88a391680f53f
David Love <david.love.wii@gmail.com>
2025-06-28 15:47:44 -0500

    Artist statement updated

--- src/routes/artist-statement/+page.svelte
+++ src/routes/artist-statement/+page.svelte
@@ -1,10 +1,25 @@
 <main class="page-layout">
-	<div class="page-container">
-		<h1 class="page-title">Artist Statement</h1>
-		<div class="text-center">
-			<p class="text-lg text-gray-300">
-				Artist statement content will be displayed here.
+	<div class="page-container px-4">
+		<h1 class="mr-2 ml-2 text-left font-bold text-4xl">Artist Statement</h1>
+		
+			<p class="scaling-text text-gray-400 indent-8 mr-2 ml-2 text-left mb-1">
+In my 21 years of life, creation has been the most intrinsic act.
+I was born and raised in Illinois, not affluent Illinois, fields of nothing Illinois, abandoned homes on every street Illinois. Art was my way of giving meaning to an environment that felt lifeless. People are born and die there, and the simple act of living is silent.
 			</p>
-		</div>
+			<p class="scaling-text text-gray-400 indent-8 mr-auto ml-2 text-left mb-1">
+Art has given me a way to define and value the beauty that surrounds me. 
+In a sunrise I have seen every morning, in the cigarrete butts that line the streets. Believing that the world is worth painting opens my eyes to the simple beauty of life. My thoughts and feelings become contextualized in the visual imagery that I subconsciously collect.
+I can no longer imagine a world in which I am not an artist first and foremost. It would be in bad faith to say I was destined for anything else. Even if I were to work a desk job, within my mind would be the workings and hopes of an artist. The intrinsic desire to mold what takes place around me into creative output. 
+			</p>
+			<p class="scaling-text text-gray-400 indent-8 mr-auto ml-2 text-left mb-1">
+My work reflects the inherent ritual I partake in when I create.
+The act of meditating on ideas, narrating abstract feelings, and visualizing thoughts becomes my spiritual practice. Just as esoteric practices throughout history are exercises in investigating and glorifying inherent truths and secret knowledge of our existence, my work mirrors this. Meaning in my work emerges in the visual icons I choose. My conscious choices in what I include and place in conversation with other elements create an unspoken scripture. Scripture that speaks to an unshakable experience, sigils consecrated in time and subjective sight. In doing so I build a lexicon of truth, meaning, and narrative of the otherwise meaningless and chaotic nature of being. I organize and describe existence, studying what lies beneath the physical veil, searching to capture the ephemeral spirit not dissimilar to practitioners and scholars of the esoteric practices of medieval and early religion.
+			</p>
+			<p class="scaling-text text-gray-400 indent-8 mr-auto ml-2 text-left mb-1">
+The act of seeing and digesting life is the most important act someone can do.
+Many times in my life I have found myself not utilizing my ability to actively observe. I admonish myself as I will admonish someone else for not actively looking at the world. To live and wake up in a breathing world, only to stare blankly at what happens before, you is a sin. Whether or not you are an artist, I believe you as a human, have an obligation to look at the world and contextualize it. Many are too set in their conditioning to actively see. They look and listen to the U.S. two party system and vote for the party their parents told them was the right party. People reject contemporary art because it doesn’t look like a renaissance painting. Do they ever ask themselves why? Do they ever close their eyes, think about what they saw, and ask why they are filled with an indescribable hunger? I hope one day my work can speak to those people. I hope to reach out to them and beg to actively look at the world, and feel joy and pride in their inherent ability to see. Our minds are pattern-seeking, we are designed to critically indulge and make meaning in a meaningless world. Those who do not act upon this are worse than killers.
+			</p>
+
+
 	</div>
 </main>
