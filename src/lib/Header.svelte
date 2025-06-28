997888d561517559fc49738e366264ae208ad848
David Love <david.love.wii@gmail.com>
2025-06-28 13:25:57 -0500

    Fix navigation links to use relative paths for GitHub Pages

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <header class="sticky top-0 z-50 w-full bg-stone-950 text-white">
 	<div class="max-w-6xl mx-auto px-6 py-4">
 		<nav class="flex justify-between items-center">
@@ -8,13 +12,13 @@
 			
 			<!-- Right side - Navigation links -->
 			<div class="flex space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8">
-				<a href="/" class="nav-link scaling-text">
+				<a href="{base}/" class="nav-link scaling-text">
 					Home
 				</a>
-				<a href="/gallery" class="nav-link scaling-text">
+				<a href="{base}/gallery" class="nav-link scaling-text">
 					Gallery
 				</a>
-				<a href="/artist-statement" class="nav-link scaling-text">
+				<a href="{base}/artist-statement" class="nav-link scaling-text">
 					Artist Statement
 				</a>
 			</div>
